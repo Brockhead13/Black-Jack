31278f42065de5af4379ba1f43c5dd243eac237c
Quinton Brockman <qbrockman.prsvr@gmail.com>
2022-11-01 22:02:55 -0400

    11-01-22

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,5 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -10,15 +10,48 @@ let sumEl = document.getElementsById("sum-el")
 let cardsEl = document.getElementsById("cards-el")
 
 function startGame(){
-    cardsEl.textContent = ""
-    sumEl.textContent ="Sum:" + sum
-    if (sum <= 20) {
+    renderGame
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20){ 
         message = "Do you want to draw a new card?"
     }else if (sum === 21){
         message = "Woohoo!  You've got Blackjack!"
-
-}else {
+    }else {
     message = "you're out of the game!"
     isAlive = false
-}
+   }
 messageEl.textContent= message
+}
+
+function newCard()
+console.log("Drawing a new card fro the deck!")
+
+let card = 6
+sum += card
+cardsEl.push(card)
+console.log(cards)
+renderGame()
+
+
+let experience = ["Certified ABA Therapist, Berea College Alumni, UT Grad School"]
+
+let messages = [
+    "Hey, how's it going?",
+    "I'm great, thank you, how about you?",
+    "All good. Been working on my protolio lately."
+]
+
+
+let newMesage = "Same here!"    
+
+
+let newMessage = "Same here!"
+let per = ["per Quinton Brockman", 48, true]
+   
+  
+console.log(messages.pop());
+console.log(messages)  

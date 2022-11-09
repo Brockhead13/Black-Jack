ef942ee03e4d75046f919959cc7558447cc9996c
Quinton Brockman <qbrockman.prsvr@gmail.com>
2022-11-08 21:53:18 -0500

    11-8-22

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,27 @@
-let firstCard = 10
-let secondCard = 4
+let firstCard = getandomCard()
+let secondCard = getRandomCard()
+let cards =[firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let message =""
+let message = ""
 let messageEl = dcument.getElementById("message-el")
 let sumEl = document.getElementsById("sum-el")
-
 let cardsEl = document.getElementsById("cards-el")
 
-function startGame(){
+function startGame() {
     renderGame
+}
+
+function getRandomCard() {
+    return 5
 }
 
 function renderGame() {
     cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20){ 
         message = "Do you want to draw a new card?"
@@ -30,28 +37,10 @@ messageEl.textContent= message
 function newCard()
 console.log("Drawing a new card fro the deck!")
 
-let card = 6
+let card = getRandomCard
 sum += card
 cardsEl.push(card)
 console.log(cards)
 renderGame()
 
 
-let experience = ["Certified ABA Therapist, Berea College Alumni, UT Grad School"]
-
-let messages = [
-    "Hey, how's it going?",
-    "I'm great, thank you, how about you?",
-    "All good. Been working on my protolio lately."
-]
-
-
-let newMesage = "Same here!"    
-
-
-let newMessage = "Same here!"
-let per = ["per Quinton Brockman", 48, true]
-   
-  
-console.log(messages.pop());
-console.log(messages)  
